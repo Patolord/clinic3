99d0261d77624ea54dfb068ab76e893ebfffb4b0
Patolord <rodrigo_luizp@hotmail.com>
2023-09-22 08:46:55 -0300

    signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,5 +1,51 @@
+import { useState } from "react";
+
+// styles
 import "./Signup.css";
 
 export default function Signup() {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="auth-form">
+      <h2>Cadastrar-se</h2>
+
+      <label>
+        <span>Nome:</span>
+        <input
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Senha:</span>
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <button className="btn">Cadastrar</button>
+    </form>
+  );
 }
