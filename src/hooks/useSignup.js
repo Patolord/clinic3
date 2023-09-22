9d1950efbed4718ebf50f50a9c97fc0cf6a7d94c
Patolord <rodrigo_luizp@hotmail.com>
2023-09-22 09:02:09 -0300

    init firestore with user collection

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,47 +1,55 @@
-import { useState, useEffect } from 'react'
-import { projectAuth } from '../firebase/config'
-import { useAuthContext } from './useAuthContext'
+import { useState, useEffect } from "react";
+import { projectAuth, projectFirestore } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
 
 export const useSignup = () => {
-  const [isCancelled, setIsCancelled] = useState(false)
-  const [error, setError] = useState(null)
-  const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const [isCancelled, setIsCancelled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
 
   const signup = async (email, password, displayName) => {
-    setError(null)
-    setIsPending(true)
-  
+    setError(null);
+    setIsPending(true);
+
     try {
       // signup
-      const res = await projectAuth.createUserWithEmailAndPassword(email, password)
+      const res = await projectAuth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
 
       if (!res) {
-        throw new Error('Could not complete signup')
+        throw new Error("Could not complete signup");
       }
 
       // add display name to user
-      await res.user.updateProfile({ displayName })
+      await res.user.updateProfile({ displayName });
+
+      // create user collection and create a document and set reference
+      await projectFirestore
+        .collection("users")
+        .doc(res.user.uid)
+        .set({ form: false, displayName });
 
       // dispatch login action
-      dispatch({ type: 'LOGIN', payload: res.user })
+      dispatch({ type: "LOGIN", payload: res.user });
 
       if (!isCancelled) {
-        setIsPending(false)
-        setError(null)
+        setIsPending(false);
+        setError(null);
       }
-    } 
-    catch(err) {
+    } catch (err) {
       if (!isCancelled) {
-        setError(err.message)
-        setIsPending(false)
+        setError(err.message);
+        setIsPending(false);
       }
     }
-  }
+  };
 
   useEffect(() => {
-    return () => setIsCancelled(true)
-  }, [])
+    return () => setIsCancelled(true);
+  }, []);
 
-  return { signup, error, isPending }
-}
+  return { signup, error, isPending };
+};
