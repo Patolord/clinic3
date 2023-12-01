9c14086b23554fa7da1e1853ea49a9a6b11bba5e
patolord <rodrigo_luizp@hotmail.com>
2023-12-01 00:30:54 -0300

    added search functionality

--- src/pages/listings/Listings.js
+++ src/pages/listings/Listings.js
@@ -1,8 +1,10 @@
 import { useCollection } from "../../hooks/useCollection";
 import { useAuthContext } from "../../hooks/useAuthContext";
+import { useState } from "react";
 
 // components
 import Fichas from "../../components/Fichas";
+import Search from "./Search";
 
 // styles
 import "./Listings.css";
@@ -10,17 +12,26 @@ import "./Listings.css";
 export default function Listings() {
   const { documents, error } = useCollection("fichas");
   const { user } = useAuthContext();
+  const [searchTerm, setSearchTerm] = useState("");
 
   console.log(user.uid);
   console.log(user);
 
+  const filteredDocuments = documents?.filter((document) => {
+    // Assuming 'title' is the field you want to search by. Adjust as needed. <input type="text" placeholder="Pesquisar" onChange={handleSearch} />
+    return document.nome.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div>
-      <h2 className="page-title">Fichas</h2>
-
-      {error && <p className="error">{error}1</p>}
-
-      {documents && <Fichas fichas={documents} />}
+      <div className="search">
+        <h2 className="page-title">Fichas cadastradas</h2>
+        <Search onSearch={setSearchTerm} />
+      </div>
+      <div>
+        {error && <p className="error">{error}</p>}
+        {filteredDocuments && <Fichas fichas={filteredDocuments} />}
+      </div>
     </div>
   );
 }
